97e81507d7c5618e4627731f4e78b705a043f007
gilge <gil.gerassi@gmail.com>
2018-02-12 10:26:42 -0500

    update code

--- jenkins-maven-pipeline/src/main/java/com/gilge/testing/Suz722.java
+++ jenkins-maven-pipeline/src/main/java/com/gilge/testing/Suz722.java
@@ -5,8 +5,8 @@ public class Suz722 {
     public String play(int number) {
 
         if (number == 2) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Suz";
-        if (number % 5 == 0) return "722";
+        if (number % 3 == 2) return "Suz";
+        if (number % 5 == 3) return "722";
 
 
         return String.valueOf(number);
